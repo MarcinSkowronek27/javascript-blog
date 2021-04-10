7eddd2d410118e6aaec0b3ab9576b8537df28543
Marcin Skowronek <marcinfoto35@op.pl>
2021-04-10 22:03:13 +0200

    Running blog

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
     console.log('links:', links);
   });*/
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
@@ -24,9 +25,12 @@ const titleClickHandler = function (event) {
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
+  targetArticle.classList.add('active');
+  console.log(targetArticle);
   /* add class 'active' to the correct article */
 }
 const links = document.querySelectorAll('.titles a');
